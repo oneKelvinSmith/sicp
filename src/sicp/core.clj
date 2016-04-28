215611e4ba76bdd0ce64845be0c8589b5d1fc9f8
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-04-28 21:29:46 +0100

    Add evaluating combinations examples

--- src/sicp/core.clj
+++ src/sicp/core.clj
@@ -5,3 +5,11 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn square [n] (* n n))
+
+(defn sum-of-squares [x y]
+  (+ (square x) (square y)))
+
+(defn f [a]
+  (sum-of-squares (+ a 1) (* a 2)))
--- test/sicp/core_test.clj
+++ test/sicp/core_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [sicp.core :refer :all]))
 
-(deftest main-prints-to-stdout
-  (testing "main prints out"
+(deftest main-test
+  (testing "main prints out Hello, World!"
     (is (= (-main) (println "Hello, World!")))))
+
+(deftest square-test
+  (testing "squaring a number"
+    (is (= (square 21) 441))
+    (is (= (square (+ 2 5)) 49))
+    (is (= (square (square 3)) 81))))
+
+(deftest sum-of-squares-test
+  (testing "summing two squares"
+    (is (= (sum-of-squares 3 4) 25))
+    (is (= (sum-of-squares 5 7) 74))))
+
+(deftest f-test
+  (testing "complex function"
+    (is (= (f 5) 136))))
