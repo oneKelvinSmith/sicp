f582860555a7d1fa6eebba93b840d59da3e67135
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-05-08 22:34:22 +0100

    Add first pass at fibonacci function

--- src/sicp/chapter_1_2.clj
+++ src/sicp/chapter_1_2.clj
@@ -1,11 +1,17 @@
 (ns sicp.chapter-1-2)
 
-(defn factorial [n]
-  (fact-iter 1 1 n))
-
 (defn fact-iter [product counter max-count]
   (if (> counter max-count)
     product
     (fact-iter (* counter product)
                (+ counter 1)
                max-count)))
+
+(defn factorial [n]
+  (fact-iter 1 1 n))
+
+(defn fib [n]
+  (cond (= n 0) 0
+        (= n 1) 1
+        :else (+ (fib (- n 1))
+                 (fib (- n 2)))))
--- test/sicp/chapter_1_2_test.clj
+++ test/sicp/chapter_1_2_test.clj
@@ -9,5 +9,14 @@
     (is (= (factorial 3) 6))
     (is (= (factorial 4) 24))
     (is (= (factorial 5) 120))
-    (is (= (factorial 6) 720))
-    ))
+    (is (= (factorial 6) 720))))
+
+(deftest fibonacci-test
+  (testing "finding the nth a fibonacci numbers"
+    (is (= (fib 0) 0))
+    (is (= (fib 1) 1))
+    (is (= (fib 2) 1))
+    (is (= (fib 3) 2))
+    (is (= (fib 4) 3))
+    (is (= (fib 5) 5))
+    (is (= (fib 6) 8))))
