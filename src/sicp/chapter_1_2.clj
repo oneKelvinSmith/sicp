d251bd900b9eecada61fd0c18d4ea7ad129a87bd
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-05-08 13:59:16 +0100

    Convert factorial to an iterative process

--- src/sicp/chapter_1_2.clj
+++ src/sicp/chapter_1_2.clj
@@ -1,6 +1,11 @@
 (ns sicp.chapter-1-2)
 
 (defn factorial [n]
-  (if (= n 1)
-    1
-    (* n (factorial (- n 1)))))
+  (fact-iter 1 1 n))
+
+(defn fact-iter [product counter max-count]
+  (if (> counter max-count)
+    product
+    (fact-iter (* counter product)
+               (+ counter 1)
+               max-count)))
