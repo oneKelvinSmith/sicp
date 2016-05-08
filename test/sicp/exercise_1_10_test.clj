90ace8e9286358749373d04213542750cc0ea2cf
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-05-08 22:24:09 +0100

    Add exercise 1.10 part 2

--- test/sicp/exercise_1_10_test.clj
+++ test/sicp/exercise_1_10_test.clj
@@ -7,3 +7,18 @@
     (is (= (A 1 10) 1024))
     (is (= (A 2 4) 65536))
     (is (= (A 3 3) 65536))))
+
+(deftest precise-mathematical-definitions-test
+  (testing "function f(n) -> 2*n"
+    (is (= (f 2) 4))
+    (is (= (f 4) 8))
+    (is (= (f 42) 84)))
+  (testing "function g(n) -> 2^n"
+    (is (= (f 2) 4))
+    (is (= (f 4) 8))
+    (is (= (f 42) 84)))
+  (testing "function h(n) -> 2^n"
+    (is (= (f 2) 4))
+    (is (= (f 4) 8))
+    (is (= (f 42) 84)))
+  )
