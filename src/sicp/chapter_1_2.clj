74e8c46cfb70f3e4c9d025e579e23aee164d0731
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-05-12 21:46:24 +0100

    Make fibonacci generator iterative

--- src/sicp/chapter_1_2.clj
+++ src/sicp/chapter_1_2.clj
@@ -10,8 +10,10 @@
 (defn factorial [n]
   (fact-iter 1 1 n))
 
+(defn fib-iter [a b count]
+  (if (= count 0)
+    b
+    (fib-iter (+ a b) a (- count 1))))
+
 (defn fib [n]
-  (cond (= n 0) 0
-        (= n 1) 1
-        :else (+ (fib (- n 1))
-                 (fib (- n 2)))))
+  (fib-iter 1 0 n))
